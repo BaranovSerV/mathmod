d25fa5dcef7fd1b967310f238bcd8cf3797ec14e
BaranovSerV <BaranovSerV>
2024-11-21 21:53:53 +0300

    116

--- linear_systems.py
+++ linear_systems.py
@@ -62,6 +62,35 @@ def jacobi(A: np.ndarray, b: np.ndarray, epsilon=1e-6, norma=1):
         
         x = x_new.copy()
 
+def relaxation_method(A, b, omega=1.0, epsilon=1e-6, norma=1):
+    """
+    Решение СЛАУ методом релаксации (SOR).
+    
+    :param A: Матрица коэффициентов (n x n).
+    :param b: Вектор правой части (n).
+    :param omega: Параметр релаксации (по умолчанию 1.0 — метод Зейделя).
+    :param tol: Точность решения (по умолчанию 1e-6).
+    :return: Приближенное решение x.
+    """
+    iteration_count = 0
+    n = len(b)
+    x = np.zeros_like(b, dtype=np.float64)  
+    
+    while True:
+        x_new = np.copy(x)  
+        iteration_count += 1
+        
+        for i in range(n):
+            s1 = sum(A[i, j] * x_new[j] for j in range(i))      
+            s2 = sum(A[i, j] * x[j] for j in range(i + 1, n)) 
+            
+            x_new[i] = (1 - omega) * x[i] + (omega / A[i, i]) * (b[i] - s1 - s2)
+        
+        if np.linalg.norm(x_new - x, norma) < epsilon:
+            return x_new, iteration_count
+        
+        x = x_new
+
 
 
 def cholecky(A: np.ndarray, b: np.ndarray) -> np.ndarray:
@@ -218,3 +247,5 @@ def gauss_partial_pivot(a, b):
         x[i] = (b[i] - np.dot(a[i, i + 1:], x[i + 1:])) / a[i, i]
     
     return x
+
+    
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from linear_systems import three_diag, jacobi, gauss_zeydel
+from linear_systems import three_diag, jacobi, gauss_zeydel, relaxation_method
 
 import numpy as np
 
@@ -10,8 +10,8 @@ A = np.array(
 
 b = np.array([2, 3.3, 2.6, 7.2])
 
-print(three_diag(A,b))
+print(jacobi(A,b, norma=np.inf))
 
-print(jacobi(A,b))
+print(gauss_zeydel(A,b, norma=np.inf))
 
-print(gauss_zeydel(A,b))
+print(relaxation_method(A,b, omega=0.9, norma=np.inf))
