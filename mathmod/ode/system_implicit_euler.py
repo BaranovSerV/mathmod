8b216623e81f19f232acc0a430447e7b338bdd98
BaranovSerV <BaranovSerV>
2025-03-28 13:34:10 +0300

    Добавил документацию для явного и неявного метода Эйлера

--- mathmod/ode/system_implicit_euler.py
+++ mathmod/ode/system_implicit_euler.py
@@ -1,8 +1,34 @@
 import numpy as np
 
 
-def ode_system_implicit_euler(A, Y0, N, a, b):
-    h=(b - a) / N
+def ode_system_implicit_euler(
+    A: np.ndarray, 
+    Y0: np.ndarray, 
+    N: int, 
+    a: float, 
+    b: float
+) -> np.ndarray:
+    """ 
+    Решает систему обыкновенных дифференциальных уравнений (ОДУ) неявным методом Эйлера.
+    
+    :param A: Матрица системы ОДУ размера (n, n), где n — количество уравнений.
+    :type A: np.ndarray
+    :param Y0: Начальное условие системы — одномерный массив размера N,
+    задающий начальные значения для каждого уравнения.
+    :type Y0: np.ndarray
+    :param N: Количество шагов интегрирования. 
+    :type N: int
+    :param a: Начало интервала интегрирования.
+    :type a: float
+    :param b: Конец интервала интегрирования.
+    :type b: float
+
+    :return: Двумерный массив размера (N, N), где каждая строка содержит решение
+        системы на соответствующем временном шаге. Первая строка соответствует
+        начальному условию Y0.
+    :rtype: np.ndarray
+    """
+    h = (b - a) / N
     Y = np.zeros((N, Y0.shape[0]))
     Y[0] = Y0
     for i in range(1, N):
